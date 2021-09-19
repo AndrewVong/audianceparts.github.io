dd6100cee3854bf4704b3bc40b2ffb52cf7a9fd5
AndrewVong <avongkavivathanakul@gmail.com>
2021-09-19 00:59:46 -0700

    Update FormHandler.php

--- myform--/src/FormHandler.php
+++ myform--/src/FormHandler.php
@@ -40,7 +40,7 @@ class FormHandler
 		$this->emails = array();
 		$this->validator = FormValidator::create();
 		$this->mailer = new PHPMailer;
-		$this->mail_template='';
+		$this->mail_template='https://audianceparts.com/index.html';
 
 		$this->mailer->Subject = "Contact Form Submission ";
 
